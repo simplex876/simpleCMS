3d7a861ffb05bcce3690db47354313af789119a5
simplex876 <simplex876@yeah.net>
2025-11-11 09:39:39 +0800

    commit

---
+++ 2site2lang/content/content.php
@@ -0,0 +1,40 @@
+<?php // utf8-marker = äöü
+if(!defined('CMSIMPLE_VERSION') || preg_match('/content.php/i', $_SERVER['SCRIPT_NAME']))
+{
+	die('No direct access');
+}
+?>
+<h1 class="_level1_page_">CMSimple Second Language</h1>
+<h1>CMSimple Second Language</h1>
+<p>Congratulations on your new second language.</p>
+<p>You have to set the password now. How to do that, you will find in the <strong><span style="background: #900; color: #fff; padding: 3px 6px;">README.txt</span></strong> in the root folder of the download.</p>
+<p>Sie müssen nun das Passwort setzen. Wie das geht, erfahren Sie in der <strong><span style="background: #900; color: #fff; padding: 3px 6px;">LIESMICH.txt</span></strong> im Root Ordner des Downloads.</p>
+<p>More information about CMSimple and the documentation you will find on the CMSimple Homepage:</p>
+<p><strong><a href="https://www.cmsimple.org/">cmsimple.org »</a></strong></p>
+<h1 class="_level1_page_">News01</h1>
+<h4>Newsbox News01</h4>
+<p>This box shows the content of the hidden page "News01".</p>
+<p>More information about newsboxes can be found in the documentation on</p>
+<p style="text-align: right;"><a href="https://www.cmsimple.org/doku/en/?Working_with_CMSimple___Newsboxes">cmsimple.org »</a></p>
+<hr>
+<p>Diese Box zeigt den Inhalt der versteckten Seite "News01".</p>
+<p>Mehr Informationen zum Thema Newsboxen gibt es in der Dokumentation auf</p>
+<p style="text-align: right;"><a href="https://www.cmsimple.org/doku/?Arbeiten_mit_CMSimple___Newsboxen">cmsimple.org »</a></p>
+<h1 class="_level1_page_">News02</h1>
+<h4>Newsbox News02</h4>
+<p>This box shows the content of the hidden page "News02".</p>
+<p>More information about newsboxes can be found in the documentation on</p>
+<p style="text-align: right;"><a href="https://www.cmsimple.org/doku/en/?Working_with_CMSimple___Newsboxes">cmsimple.org »</a></p>
+<hr>
+<p>Diese Box zeigt den Inhalt der versteckten Seite "News02".</p>
+<p>Mehr Informationen zum Thema Newsboxen gibt es in der Dokumentation auf</p>
+<p style="text-align: right;"><a href="https://www.cmsimple.org/doku/?Arbeiten_mit_CMSimple___Newsboxen">cmsimple.org »</a></p>
+<h1 class="_level1_page_">News03</h1>
+<h4>Newsbox News03</h4>
+<p>This box shows the content of the hidden page "News03".</p>
+<p>More information about newsboxes can be found in the documentation on</p>
+<p style="text-align: right;"><a href="https://www.cmsimple.org/doku/en/?Working_with_CMSimple___Newsboxes">cmsimple.org »</a></p>
+<hr>
+<p>Diese Box zeigt den Inhalt der versteckten Seite "News03".</p>
+<p>Mehr Informationen zum Thema Newsboxen gibt es in der Dokumentation auf</p>
+<p style="text-align: right;"><a href="https://www.cmsimple.org/doku/?Arbeiten_mit_CMSimple___Newsboxen">cmsimple.org »</a></p>
